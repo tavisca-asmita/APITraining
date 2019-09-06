312d325f36f55fccb330ea0ea2e1a655a74103fc
Asmita Sharma <assharma@affinionds.com>
2019-09-06 10:22:50 +0530

    More test cases added

--- WebAPITests/ServicesTests.cs
+++ WebAPITests/ServicesTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Xunit;
-using WebApi;
 using WebApi.Services;
 using WebApi.Model;
 using WebApi.Data;
@@ -33,7 +31,7 @@ namespace WebAPITests
             }
             Assert.True(isSame);  
         }
-
+         
         [Fact]
         public void GetByIdTest()
         {
@@ -46,7 +44,7 @@ namespace WebAPITests
                 Author = "C# Author",
                 Category = "Programming Language"
             };
-            Assert.Equal(expectedBook, BookService.Get(1));
+            Assert.Equal(expectedBook.Id, book.Id);
         }
 
         [Fact]
@@ -62,6 +60,74 @@ namespace WebAPITests
             };
             int status = BookService.Post(book);
             Assert.Equal(1, status);
+        }
+
+        [Fact]
+        public void NegativeIdPostTest()
+        {
+            Book book = new Book
+            {
+                Id = -3,
+                Name = "Java",
+                Author = "George",
+                Category = "Programming",
+                Price = 100
+            };
+            Assert.Equal(-1, BookService.Post(book));
+        }
+
+        [Fact]
+        public void StringWithNumberPostTest()
+        {
+            Book book = new Book
+            {
+                Id = 3,
+                Name = "Jav1a",
+                Author = "George",
+                Category = "Programming",
+                Price = 100
+            };
+            Assert.Equal(-1, BookService.Post(book));
+        }
+
+        [Fact]
+        public void AlreadyPresentPostTest()
+        {
+            Book book = new Book { Name = "C#", Id = 1, Price = 50, Author = "C# Author", Category = "Programming Language" };
+            Assert.Equal(0, BookService.Post(book));
+        }
+
+        [Fact]
+        public void PutTest()
+        {
+            Book book = new Book { Name = "C#", Id = 1, Price = 60, Author = "C# Author", Category = "Programming Language" };
+            Assert.Equal(1, BookService.Put(1, book));
+        }
+
+        [Fact]
+        public void NegativeIdPutTest()
+        {
+            Book book = new Book { Name = "C#", Id = -1, Price = 60, Author = "C# Author", Category = "Programming Language" };
+            Assert.Equal(-1, BookService.Put(-1, book));
+        }
+
+        [Fact]
+        public void NoContentPutTest()
+        {
+            Book book = new Book { Name = "C#", Id = 1, Price = 60, Author = "C# Author", Category = "Programming Language" };
+            Assert.Equal(0, BookService.Put(6, book));
+        }
+
+        [Fact]
+        public void DeleteTest()
+        {
+            Assert.Equal(1, BookService.Delete(1));
+        }
+
+        [Fact]
+        public void IdNotPresentDeleteTest()
+        {
+            Assert.Equal(0, BookService.Delete(8));
         }
     }
 }
