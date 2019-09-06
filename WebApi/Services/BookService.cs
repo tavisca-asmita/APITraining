51404b37e7488fcdf8b13c38b7d559b8f642b0ba
Asmita Sharma <assharma@affinionds.com>
2019-09-06 10:34:30 +0530

    Validation Changes

--- WebApi/Services/BookService.cs
+++ WebApi/Services/BookService.cs
@@ -55,29 +55,10 @@ namespace WebApi.Services
 
         public int Post(Book book)
         {
-            bool flag = true;
-            if (book.Id <= 0 || book.Price <= 0)
+            if (book.Id <= 0 || book.Price <= 0 || !book.Name.All(char.IsLetter) || !book.Author.All(char.IsLetter) || !book.Category.All(char.IsLetter))
                 return -1;
-            else if (book.Name.Contains('0') || book.Name.Contains('1') ||
-                    book.Name.Contains('2') || book.Name.Contains('3') ||
-                    book.Name.Contains('4') || book.Name.Contains('5') ||
-                    book.Name.Contains('6') || book.Name.Contains('7') ||
-                    book.Name.Contains('8') || book.Name.Contains('9'))
-                return -1;
-            else if (book.Author.Contains('0') || book.Author.Contains('1') ||
-                    book.Author.Contains('2') || book.Author.Contains('3') ||
-                    book.Author.Contains('4') || book.Author.Contains('5') ||
-                    book.Author.Contains('6') || book.Author.Contains('7') ||
-                    book.Author.Contains('8') || book.Author.Contains('9'))
-                return -1;
-            else if (book.Category.Contains('0') || book.Category.Contains('1') ||
-                    book.Category.Contains('2') || book.Category.Contains('3') ||
-                    book.Category.Contains('4') || book.Category.Contains('5') ||
-                    book.Category.Contains('6') || book.Category.Contains('7') ||
-                    book.Category.Contains('8') || book.Category.Contains('9'))
-                return -1;
-
-            if (flag)
+            
+            else
             {
                 foreach(var item in BookList)
                 {
